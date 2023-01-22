021bd07332e40e4368bab6dec566bd6dc521f5ba
Ivan Voxgaard <ivan.voxgaard@gmail.com>
2023-01-22 00:04:21 -0500

    +arguments

--- p_w_m.py
+++ p_w_m.py
@@ -1,10 +1,27 @@
+import math
+
 class Point:
     def reset(self):
-        self.x = 0
-        self.y = 0
+        self.move(0, 0)
 
-p = Point()
+    def move(self, x, y):
+        self.x = x
+        self.y = y
 
-p.reset()
+    def calculate_distance(self, other_point):
+        return math.sqrt((self.x-other_point.x)**2+(self.y-other_point.y)**2)
 
-print(p.x, p.y)
+point1 = Point()
+point2 = Point()
+
+point1.reset()
+point2.move(5,0)
+
+print(point2.calculate_distance(point1))
+
+assert (point2.calculate_distance(point1) == point1.calculate_distance(point2))
+
+point1.move(3,4)
+
+print(point1.calculate_distance(point2))
+print(point1.calculate_distance(point1))
