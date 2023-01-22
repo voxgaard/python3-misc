6f8513b865ec0c3706d0ad2f15dd6d6a639683f2
Ivan Voxgaard <ivan.voxgaard@gmail.com>
2023-01-22 02:17:21 -0500

    +docstrings

--- p_w_m.py
+++ p_w_m.py
@@ -2,17 +2,31 @@ import math
 
 class Point:
 
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
+        '''Initialize the position of a new point. The x and y
+
+        coordinates can be specified. If they are not, the
+
+        point defaults to the origin.'''
         self.move(x, y)
 
     def reset(self):
+        'Reset the point back to the geometric origin: 0, 0'
         self.move(0, 0)
 
     def move(self, x, y):
+        "Move the point to a new location in 2D space."
         self.x = x
         self.y = y
 
     def calculate_distance(self, other_point):
+        """Calculate the distance from this point to a second point passed as a parameter.
+
+        This function uses the Pythagorean Theorem to calculate
+
+        the distance between the two points. The distance is
+
+        returned as a float."""
         return math.sqrt((self.x-other_point.x)**2+(self.y-other_point.y)**2)
 
 """
