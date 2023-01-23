4ffadf6d3a8940d34d13ec91a18a4762eff0572e
Ivan Voxgaard <ivan.voxgaard@gmail.com>
2023-01-23 01:22:25 -0500

    +partial menu

---
+++ python_notebook/menu.py
@@ -0,0 +1,46 @@
+import sys
+
+from notebook import Notebook, Note
+
+class Menu:
+    '''Display a menu and respond to choices when run.'''
+    def __init__(self):
+        self.notebook = Notebook()
+        self.choices = {
+            "1": self.show_notes,
+            "2": self.search_notes,
+            "3": self.add_note,
+            "4": self.modify_note,
+            "5": self.flee
+        }
+
+    def display_menu(self):
+        print("""
+            Notebook Menu
+            1. Show all Notes
+            2. Search Notes
+            3. Add Note
+            4. Modify Note
+            5. Flee from the Notes
+            """)
+    
+    def run(self):
+        '''Display the menu and respond to choices.'''
+        while True:
+            self.display_menu()
+            choice = input("Enter an option: ")
+            action = self.choices.get(choice)
+            if action:
+                action()
+            else:
+                print("{0} is not a valid choice".format(choice))
+
+    def show_notes(self, notes=None):
+        if not notes:
+            notes = self.notebook.notes
+        for note in notes:
+            print("{0}: {1}\n{2}".format(
+                note.id, note.tags, note.memo))
+
+    def search_notes(self):
+        
